6a65fcd53b89eb9c0a20a71ff3d628a19172d5fc
vireetha <dudalaviree69@gmail.com>
2024-10-15 06:33:58 +0530

    20

---
+++ lesson1/squareroot.py
@@ -0,0 +1,11 @@
+num= 49
+num_sqrt=(num)
+
+print(f'{num} square root is{num_sqrt}')
+
+
+num = int(input("enter the number:"))
+
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
