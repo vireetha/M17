4429208439a7f8e3fcb7b2b7e7b4cae58fcd5bb6
vireetha <dudalaviree69@gmail.com>
2024-10-15 06:30:47 +0530

    19

---
+++ lesson1/Firebrigade.py
@@ -0,0 +1,12 @@
+
+number=int(input("Enter a number   "))
+
+
+if number>0:
+  print(number,"is a positive number")
+elif number==0:
+  print(number,"is a neutral number")
+else:
+  print("Negative number")
+a=True
+print(not a)
